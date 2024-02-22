a01072ee539e65aaec7e1403f33b2cd27ae0c5cd
Robin Ilyk <contact@robko.cz>
2024-02-22 10:48:12 +0100

    remove tabnine

--- init.lua
+++ init.lua
@@ -25,7 +25,6 @@ require("lazy").setup({
 	{ import = "user.plugins.flash" },
 	{ import = "user.plugins.gitsigns" },
 	{ import = "user.plugins.aerial" },
-	{ import = "user.plugins.tabnine" }
 }, {
 	change_detection = {
 		enabled = false,
